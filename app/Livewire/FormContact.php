aed85e2a7e1b54e4198a10b3b5b871a073c7b87b
nosrettep <abadepetterson@gmail.com>
2025-06-18 11:20:44 -0300

    Add form validation and logging for new contact submissions
    - Implemented validation rules for name, email, and phone fields in the FormContact Livewire component.
    - Added logging functionality to capture new contact details upon form submission.
    - Updated the form in the Blade view to bind input fields to Livewire properties and display validation error messages.

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+
+    public $name, $email, $phone;
+
+    public function newContact(){
+        $this->validate([
+            'name' => 'required|min:5|max:50',
+            'email' => 'required|email|min:5|max:50',
+            'phone' => 'required|min:5|max:50',
+        ]);
+
+        Log::info('Novo contato: ' . $this->name . '-' . $this->email . '-' . $this->phone);
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
