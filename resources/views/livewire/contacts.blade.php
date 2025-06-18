9f5f087e10f111ceebba23c2a46b8d4560cdefe2
nosrettep <abadepetterson@gmail.com>
2025-06-18 11:02:04 -0300

    Add Livewire components for contact management
    - Created Contacts and FormContact Livewire components for handling contact display and form submission.
    - Added corresponding Blade views for each component: contacts.blade.php and form-contact.blade.php.
    - Updated main-component.blade.php to include the new Livewire components in the layout.

---
+++ resources/views/livewire/contacts.blade.php
@@ -0,0 +1,4 @@
+<div class="card p-5">
+    <p>Contacts</p>
+    <hr class="m-0-0">
+</div>
--- resources/views/livewire/main-component.blade.php
+++ resources/views/livewire/main-component.blade.php
@@ -2,4 +2,16 @@
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     <div class="text-center my-5">
         <img src="{{ asset('assets/images/logo.png') }}" alt='logo' width="128px">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                @livewire('form-contact')
+            </div>
+            <div class="col-md-8">
+                @livewire('contacts')
+            </div>
+        </div>    
+    </div>
+
 </div>
